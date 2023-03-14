d4cdb23d8f4410836b3900bbe254c5478ab3b4a2
rafaeldgeo <94787506+rafaeldgeo@users.noreply.github.com>
2023-03-14 16:39:20 -0300

    desenvolvendo

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+// validação customizada do bootstrap para os campos select 1ª e 2ª validação
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
