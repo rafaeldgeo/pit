3a4ae78564f01e1bd712984d60f97eeafa2ac3ff
rafaeldgeo <94787506+rafaeldgeo@users.noreply.github.com>
2023-03-21 14:10:13 -0300

    em desenvolvimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 
-
-
 // validação customizada do bootstrap para os campos select 1ª e 2ª validação
 (() => {
     'use strict'
@@ -18,3 +16,8 @@
       }, false)
     })
   })()
+
+// tooltip com o nome da linha 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
